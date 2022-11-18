088a4e9063e806fc3f1b0c8c062941fb11352c5e
geodevcode <olatunde336@gmail.com>
2022-11-18 08:21:46 +0100

    first commit

---
+++ src/components/Typography.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Hero = styled.h1`
+  font-size: 44px
+  font-family: poppins;
+  font-weight: italic;
+  color: white;
+  border: 2px solid black;
+  
+`;
+
+export const Subheading = styled.h1`
+  font-size: 18px;
+  font-family: poppins;
+  color: blue;
+  border: 2px solid white;
+  margin-top: 30px
+`;
+
+export const Herobody = styled.h1`
+  font-size: 14px;
+  font-family: poppins;
+  color: green;
+  border: 2px solid purple;
+  margin-top: 90px;
+`;
+
+export const Headingbody = styled.h1`
+  font-size: 18px;
+  font-family: poppins;
+  color: pink;
+  border: 2px solid white;
+  margin-top: 90px
+`;
+
+export const Headingbig = styled.h1`
+  font-size: 40px;
+  font-family: poppins;
+  color: grey;
+  font-style: italic;
+  border: 2px solid white;
+  margin-top: 90px
+`;
